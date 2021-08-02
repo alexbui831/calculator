63970c6fdf5cfb8c84d7217436e47f67c0996664
Alexander Bui <alexbui831@gmail.com>
2021-08-02 02:40:06 -0500

    most of the calculator is finished, need to implement extra credit functionalities such as keyboard support, floating decimals, divide by 0.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # calculator
-A web app calculator created using  HTML, CSS, and Javascript.
+A web app calculator created using  HTML, CSS, and Javascript for the Odin Project curriculum
+
+Final Thoughts:
+As simple as I feel a calculator would function, the project was defintely harder than I expected. Setting up the HTML + CSS to create the calculator front-end was easy. I used CSS grid to layout all the buttons needed for a proper calculator.  
+
+The most challenging part of the code was the back-end Javascript. I had a hard time figuring out what values to store or what functions would be needed. It was also hard wrapping my head around how to append numbers to the display block and simultaneously compute calculations while also checking certain conditions such as having only one operator at a time. I feel like I had a harder time laying out the foundations of this code than I did the other projects I did. However, I was eager to learn how to get this calculator to work so I did a lot of research on it and finally got it done. 
+
+I am still not 100% comfortable with the code on this project but I hope in the future I will understand it easily.
--- calculator.js
+++ calculator.js
@@ -1,48 +1,138 @@
-function add(a, b) {
-    return a + b;
-}
+class Calculator {
+    constructor(previousDisplayText, currentDisplayText) {
+        this.previousDisplayText = previousDisplayText;
+        this.currentDisplayText = currentDisplayText;
+        this.readyToReset = false;
+        this.clear();
+    }
 
-function subtract(a, b) {
-    return a - b;
-}
+    clear() {
+        this.currentOperand = "";
+        this.previousOperand = "";
+        this.operation = undefined;
+    }
 
-function multiply(x, y) {
-    return x * y;
-}
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1);
+    }
 
-function divide(x, y) {
-    return x / y;
-}
+    appendNumber(number) {
+        if (number === "." && this.currentOperand.includes(".")) return
 
-function operator(operator, num1, num2) {
-    switch (operator) {
-        case "add":
-            let sum = add(num1, num2);
-            currentNum = sum;
+        this.currentOperand = this.currentOperand.toString() + number.toString();
+    }
 
-            console.log("current: " + currentNum);
-            break;
-        case "subtract":
-            let difference = subtract(num1, num2);
-            currentNum = difference;
+    chooseOperator(operation) {
+        if (this.currentOperand === "") return;
+        if (this.previousOperand != "") {
+            this.compute();
+        }
+        this.operation = operation;
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = "";
+    }
 
-            console.log("current: " + currentNum);
-            break;
-        case "multiply":
-            let product = multiply(num1, num2);
-            currentNum = product;
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case "+":
+                computation = prev + current;
+                break;
+            case "-":
+                computation = prev - current;
+                break;
+            case "x":
+                computation = prev * current;
+                break;
+            case "÷":
+                computation = prev / current;
+                break;
+            default:
+                return;
+        }
+        this.readyToReset = true;
+        this.currentOperand = computation;
+        this.operation = undefined;
+        this.previousOperand = "";
+    }
 
-            console.log("current: " + currentNum);
-            break;
-        case "divide":
-            let quotient = divide(num1, num2);
-            currentNum = quotient;
+    getDisplayNum(number) {
+        const strNum = number.toString()
+        const integerDigits = parseFloat(strNum.split(".")[0])
+        const decimalDigits = strNum.split(".")[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ""
+        }
+        else {
+            integerDisplay = integerDigits.toLocaleString("en", { maximumFractionDigits: 0 })
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        }
+        else {
+            return integerDisplay
+        }
+    }
+    update() {
+        this.currentDisplayText.innerText = this.getDisplayNum(this.currentOperand);
+        if (this.operation != null) {
+            this.previousDisplayText.innerText = `${this.getDisplayNum(this.previousOperand)} ${this.operation}`
+        }
+        else {
+            this.previousDisplayText.innerText = "";
+        }
 
-            console.log("current: " + currentNum);
-            break;
     }
 }
 
-//  global currentNum variable
-let currentNum = 0;
+const numberButtons = document.querySelectorAll("[data-number")
+const operationButtons = document.querySelectorAll("[data-operation]")
+const equalButton = document.querySelector("[data-equal]")
+const percentButton = document.querySelector("[data-percent]")
+const acButton = document.querySelector("[data-all-clear]")
+const deleteButton = document.querySelector("[data-delete]")
+
+const previousDisplayText = document.querySelector("[data-previous]")
+const currentDisplayText = document.querySelector("[data-current]")
+
+const calculator = new Calculator(previousDisplayText, currentDisplayText)
+
+//  appendNumber() function buttons
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (calculator.previousOperand === "" && calculator.currentOperand !== "" && calculator.readyToReset) {
+            calculator.currentOperand = "";
+            calculator.readyToReset = false;
+        }
+        calculator.appendNumber(button.innerText);
+        calculator.update();
+    });
+});
+
+//  chooseOperator() function buttons
+operationButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        calculator.chooseOperator(button.innerText);
+        calculator.update();
+    });
+});
+
+equalButton.addEventListener("click", button => {
+    calculator.compute();
+    calculator.update();
+});
+
+acButton.addEventListener("click", button => {
+    calculator.clear();
+    calculator.update();
+});
+
+deleteButton.addEventListener("click", button => {
+    calculator.delete();
+    calculator.update();
+})
 
